47a81ea2c0dde5c56ea00bde0976f9a03966699c
anderson <andersonpayao018@gmail.com>
2024-03-22 20:53:16 -0300

    herança

--- 02_Heranca/forma.js
+++ 02_Heranca/forma.js
@@ -1,15 +1,19 @@
 class Forma{
-    constructor(){
-        this.tipoDeForma = "Forma Abstrata";
+    constructor(tipoDeForma = "Forma Abstrata"){
+        //this.tipoDeForma = "Forma Abstrata";
+        this.tipoDeForma = tipoDeForma;
 
     }
 
     imprimeForma(){
+        try {
         alert (`Tipo de Forma: ${this.tipoDeForma} - Área: ${this.calculaArea()}`);
-        //alert(`Tipo de forma: ${this.tipoDeForma}`);
+        console.log("aqui");
+    }catch (error){
+        alert("o codigo esta com erro");
     }
 
-
+    }
     calculaArea(){
         throw new Error ("O Método CaculaArea() Deve Ser Implementado Na Subclasse.")
     }
--- 02_Heranca/quadrado.js
+++ 02_Heranca/quadrado.js
@@ -1,11 +1,12 @@
 class Quadrado extends Forma {
-    constructor (lado) {
-        super();
-        this.tipoDeForma = "Quadrado";
-        this.lado = lado;
-    }
+  constructor(lado) {
+    // super();
+    // this.tipoDeForma = "Quadrado";
+    super("Quadrado");
+    this.lado = lado;
+  }
 
-    calculaArea(){
-        return this.lado * this.lado;
-    }
+  calculaArea() {
+    return this.lado * this.lado;
+  }
 }
